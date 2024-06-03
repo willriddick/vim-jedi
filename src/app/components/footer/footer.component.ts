97f5d835580cff8e6bb6973bbc915ad25488fca8
Will Riddick <willriddick>
2024-06-03 16:11:16 -0400

    Updated styles, updated footer with buttons

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
